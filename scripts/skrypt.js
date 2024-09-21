f55e6985f3b8bb90d208c2579615a6e38706c2b3
Konrad Zatorski <kodiz2005@gmail.com>
2024-09-21 12:59:30 +0200

    Działający system zapisywania finalnej odpowiedzi użytkownika

--- scripts/skrypt.js
+++ scripts/skrypt.js
@@ -1,7 +1,10 @@
 let correctAnswers = ["program", "linux", "gmail", "rekurencja", "formatowanie", "youtube", "reinstalacja", "java", "tor", "ftp", "malware"];
 let finalAnswer = "informatyka";
+
+
 $(document).ready(function() {
     $("#submit").click(function(){
+        let userFinalAnswer = "";
         let userAnswers = []
         for (j = 0; j < correctAnswers.length; j++){
             let tempTable = [];
@@ -10,8 +13,17 @@ $(document).ready(function() {
                 tempTable[i] = document.getElementsByClassName(`q${j}`)[i].value.toLowerCase();
             };
             userAnswers[j] = tempTable;
-            console.log(tempTable);
+            for (k = 0; k < correctAnswers[j].length; k++) {
+                if (userAnswers[j][k] == correctAnswers[j][k]) {
+                    document.getElementsByClassName(`q${j}`)[k].style.backgroundColor = "green";
+                }
+                else {
+                    document.getElementsByClassName(`q${j}`)[k].style.backgroundColor = "red";
+                };
+            };
+            tempUserFinalAnswer = document.getElementsByClassName(`f${j}`)[0].value.toLowerCase();
+            userFinalAnswer += tempUserFinalAnswer;
         };
-        console.log(userAnswers);
+        console.log(userFinalAnswer);
     });
 });
