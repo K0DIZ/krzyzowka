99d2499388e1eba1122a2efa7e43de3a7d98b8d1
Konrad Zatorski <kodiz2005@gmail.com>
2024-09-22 10:19:28 +0200

    Działająca krzyżówka wraz z kolorkami

--- scripts/skrypt.js
+++ scripts/skrypt.js
@@ -1,11 +1,12 @@
-let correctAnswers = ["program", "linux", "gmail", "rekurencja", "formatowanie", "youtube", "reinstalacja", "java", "tor", "ftp", "malware"];
+let correctAnswers = ["program", "linux", "gmail", "rekurencja", "formatowanie", "youtube", "reinstalacja", "java", "tor", "tcp", "malware"];
 let finalAnswer = "informatyka";
 
 
 $(document).ready(function() {
     $("#submit").click(function(){
-        let userFinalAnswer = "";
+        let userFinalAnswer = ["","","","","","","","","","",""];
         let userAnswers = []
+        isCorrect = true;
         for (j = 0; j < correctAnswers.length; j++){
             let tempTable = [];
             for (i = 0; i < correctAnswers[j].length; i++) {
@@ -19,11 +20,48 @@ $(document).ready(function() {
                 }
                 else {
                     document.getElementsByClassName(`q${j}`)[k].style.backgroundColor = "red";
+                    isCorrect = false;
                 };
             };
             tempUserFinalAnswer = document.getElementsByClassName(`f${j}`)[0].value.toLowerCase();
-            userFinalAnswer += tempUserFinalAnswer;
+            document.getElementsByClassName(`p${j}`)[0].innerHTML = tempUserFinalAnswer;
+            userFinalAnswer[j] = tempUserFinalAnswer;
         };
+            for (f = 0; f < finalAnswer.length; f++) {
+                if (finalAnswer[f] == userFinalAnswer[f]) {
+                    document.getElementsByClassName(`p${f}`)[0].style.backgroundColor = "green";
+                }
+                else {
+                    document.getElementsByClassName(`p${f}`)[0].style.backgroundColor = "red";
+                    isCorrect = false;
+                };
+            };
+        result = document.getElementById("p-user-final-result");
         console.log(userFinalAnswer);
+        console.log(finalAnswer);
+        console.log(isCorrect);
+        for (d = 0; d < finalAnswer.length; d++) {
+            if (userFinalAnswer[d] != finalAnswer[d]) {
+                isCorrect = false;
+            }
+        };
+        if (isCorrect) {
+            result.innerHTML = "Gratulacje! Krzyżówka rozwiązana poprawnie!";
+            result.style.color = "green";
+        }
+        else {
+            result.innerHTML = "Nie wszystkie odpowiedzi są poprawne!";
+            result.style.color = "red";
+        };
+    });
+    $("#reset").click(function() {
+        for (c = 0; c < finalAnswer.length; c++) {
+            document.getElementsByClassName(`p${c}`)[0].innerHTML = "";
+            document.getElementsByClassName(`p${c}`)[0].style.backgroundColor = "white";
+            for (j = 0; j < correctAnswers[c].length; j++){
+                document.getElementsByClassName(`q${c}`)[j].style.backgroundColor = "white";
+            };
+        };
+        result.innerHTML = "";
     });
 });
